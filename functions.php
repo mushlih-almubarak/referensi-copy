ec32c169f17c3ad5f2e055fb321c0248f4a28a7b
Mushlih Almubarak <mushlih2004@gmail.com>
2020-10-09 11:02:39 +0300

    Kode untuk menyertakan link referensi otomatis
    Kode ini berfunsi untuk menyertakan link referensi otomatis pada teks postingan yang di copy

---
+++ functions.php
@@ -0,0 +1,36 @@
+function add_copyright_text() {
+    if (is_single()) { ?>
+ 
+<script type='text/javascript'>
+function addLink() {
+    if (
+window.getSelection().containsNode(
+document.getElementsByClassName('entry-content')[0], true)) {
+    var body_element = document.getElementsByTagName('body')[0];
+    var selection;
+    selection = window.getSelection();
+    var oldselection = selection
+    var pagelink = "<br /><br /> Baca selanjutnya di Mutiara Dakwah: <?php the_title(); ?> <a href='<?php echo wp_get_shortlink(get_the_ID()); ?>'><?php echo wp_get_shortlink(get_the_ID()); ?></a>"; //Ubah ini jika Anda suka
+    var copy_text = selection + pagelink;
+    var new_div = document.createElement('div');
+    new_div.style.left='-99999px';
+    new_div.style.position='absolute';
+ 
+    body_element.appendChild(new_div );
+    new_div.innerHTML = copy_text ;
+    selection.selectAllChildren(new_div );
+    window.setTimeout(function() {
+        body_element.removeChild(new_div );
+    },0);
+}
+}
+ 
+ 
+document.oncopy = addLink;
+</script>
+ 
+<?php
+}
+}
+ 
+add_action( 'wp_head', 'add_copyright_text');
